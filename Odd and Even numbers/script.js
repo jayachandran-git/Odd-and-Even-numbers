65218f722492154bfb7bf23c1847ac40e23e9e15
jayachandran-git <148688494+jayachandran-git@users.noreply.github.com>
2024-02-01 15:22:23 +0530

    Add files via upload

---
+++ Odd and Even numbers/script.js
@@ -0,0 +1,23 @@
+let number=parseInt(prompt("Enter the number"));
+
+if(number%2==0){
+    alert(number+" is the even number");
+}else{
+    alert(number+ "is the odd number");
+}
+
+  
+
+//Display the odd number
+
+for(let i=1;i<=100;i+=2){
+    console.log(i);
+}
+
+//Display the even number
+
+for(let x=1; x<=100;x++){
+    if(x%2==0){
+        console.log(x);
+    }
+}
